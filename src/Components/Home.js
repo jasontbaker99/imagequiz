1c921c526aabe2f2a4e592488b6b59a825fb0161
= <=>
2020-10-09 00:02:58 -0700

    pictures

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './Home.css';
-import { Link } from "react-router-dom";
-import daffodilimg from "/images/daffodil.png"
+import { Link } from "react-router-dom"
 
 class Home extends React.Component {
 
@@ -35,10 +34,10 @@ class Home extends React.Component {
         ];
 
         function drawPics(f) {
-            //let p = "/images/"+f.picture;
+            let p = "/images/"+f.picture;
             return (
                 <div>
-                    <img className="image" src={daffodilimg} alt="" />
+                    <img className="image" src={p} alt="" />
                     <br/>
                     {f.name}
                 </div>
@@ -64,6 +63,13 @@ class Home extends React.Component {
                 </div>
                 <div className="Pics">
                     {drawPics(flowers[0])}
+                    {drawPics(flowers[1])}
+                    {drawPics(flowers[2])}
+                    {drawPics(flowers[3])}
+                    {drawPics(flowers[4])}
+                    {drawPics(flowers[5])}
+                    {drawPics(flowers[6])}
+                    {drawPics(flowers[7])}
                 </div>
             </div>
         );
